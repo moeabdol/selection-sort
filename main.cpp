e23751994474a164804b768d6f7d68a3e4075ee3
moeabdol <mohd.a.saed@gmail.com>
2021-05-28 19:16:50 +0300

    Implement selection sort

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int> * numbers) {
+    if (numbers->size() == 0 || numbers->size() == 1)
+        return;
+
+    for (int i = 0; i < numbers->size(); i++)
+        for (int j = i + 1; j < numbers->size(); j++)
+            if (numbers->at(j) < numbers->at(i))
+                swap(numbers->at(j), numbers->at(i));
+}
+
+int main() {
+    vector<int> *numbers = new vector<int>(0);
+
+    numbers->push_back(3);
+    numbers->push_back(2);
+    numbers->push_back(5);
+    numbers->push_back(1);
+    numbers->push_back(4);
+
+    selectionSort(numbers);
+
+    for (int i = 0; i < numbers->size(); i++)
+        cout << numbers->at(i) << " ";
+    cout << endl;
+
+    return 0;
+}
