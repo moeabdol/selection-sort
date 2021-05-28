dda69ccbbc86dbae486ee3c0dbc7fc7e7e5def4d
moeabdol <mohd.a.saed@gmail.com>
2021-05-28 19:45:39 +0300

    Minor fix

--- main.cpp
+++ main.cpp
@@ -6,10 +6,17 @@ void selectionSort(vector<int> * numbers) {
     if (numbers->size() == 0 || numbers->size() == 1)
         return;
 
-    for (int i = 0; i < numbers->size() - 1; i++)
+    for (int i = 0; i < numbers->size() - 1; i++) {
+        int smallest = i;
+
         for (int j = i + 1; j < numbers->size(); j++)
-            if (numbers->at(j) < numbers->at(i))
-                swap(numbers->at(j), numbers->at(i));
+            if (numbers->at(j) < numbers->at(smallest)) {
+                smallest = j;
+            }
+
+        if (numbers->at(smallest) < numbers->at(i))
+            swap(numbers->at(i), numbers->at(smallest));
+    }
 }
 
 int main() {
